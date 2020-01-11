35926d299637d9ce9140bec5142980085ef3ba2c
David Papamichael <davidpapamichael@gmail.com>
2020-01-11 14:06:34 +0000

    first test passed, feature test for form added

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra/base'
 
 class Birthday < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
 
   run! if app_file == $0
 
---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,8 @@
+feature "testing name form" do
+  scenario "testing name == user input from form" do
+    visit('/')
+    fill_in :name, with: "David"
+    click_button "Enter Name"
+    expect(page).to have_content "Happy Birthday David!"
+  end
+end
